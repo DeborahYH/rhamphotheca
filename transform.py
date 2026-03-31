30a0cb92d41b6e323f1e55fc19c1b1458888b1fd
Deborah Y Hukuda <deborah.hukuda@gmail.com>
2026-03-31 19:05:08 -0300

    Padronizacao das colunas booleanas do wiki aves

--- transform.py
+++ transform.py
@@ -1,7 +1,10 @@
 import pandas as pd
+import numpy as np
 
 
 def transform_data(df_cards):
+
+    boolean_dict = {"Sim": True, "Não": False}
 
     # Standardizes the values in the 'media_type' column to be more descriptive
     df_cards["media_type"] = df_cards["media_type"].map({'F': 'Foto', 'S': 'Som'})
@@ -9,9 +12,16 @@ def transform_data(df_cards):
     location = df_cards["location"].str.split("/", expand=True)
     df_cards["municipality"] = location[0]
     df_cards["state"] = location[1]
-    print(df_cards[["media_type", "location", "municipality", "state"]])
 
-with open ("wikiaves_data/calidris_melanotos_(30-03-2026)_cards.csv", "r", encoding="utf-8") as file:
+    boolean_columns = ["emitter_seen", "after_playback", "banded", "possible_release"]
+
+    for column in boolean_columns:
+        if column in df_cards.columns:
+            df_cards[column] = df_cards[column].map(boolean_dict)   
+        else:
+            df_cards[column] = np.nan 
+
+with open ("wikiaves_data/tachyphonus_coronatus_(31-03-2026)_cards.csv", "r", encoding="utf-8") as file:
     df_cards = pd.read_csv(file)
 
 transform_data(df_cards)
